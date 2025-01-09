dd94693b61944772312cdbd1361f9519c6682e75
Mo-fawaz <mmmf.mmmf96@gmail.com>
2025-01-09 13:38:35 +0300

    fix route

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserAuthController;
 
 
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -15,16 +14,15 @@ use App\Http\Controllers\UserAuthController;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
-
-;
-Route::post('/register',[UserAuthController::class,'register']);
-Route::post('login',[UserAuthController::class,'login']);
-Route::Post('logout',[UserAuthController::class,'logout']);
-Route::post('/upload',[UserAuthController::class,'addImage'])
-
+*/;
+Route::post('/register', [UserAuthController::class, 'register']);
+Route::post('login', [UserAuthController::class, 'login']);
+Route::Post('logout', [UserAuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/upload', [UserAuthController::class, 'addImage'])
     ->middleware('auth:sanctum');
-Route::get('token', function () {return ('Welcome to Abha');})->middleware('auth:sanctum');
+Route::get('token', function () {
+    return ('Welcome to Abha');
+})->middleware('auth:sanctum');
 
 Route::post('/send-message', [\App\Http\Controllers\ChatController::class, 'sendMessage']);
 Route::Get('/receive-message', [\App\Http\Controllers\ChatController::class, 'receiveMessage']);
