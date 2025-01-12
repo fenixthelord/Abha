90affe6b04a5a9501c50ce029266f95cc053cca2
Qousai <qousai.alkourdi@darrebni.net>
2025-01-12 09:53:31 +0300

    save

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\NotificationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/send-notification', [NotificationController::class, 'sendNotification']);
 
-    Route::post('logout',[UserController::class,'logout'])->middleware('auth:sanctum');
-Route::post('register',[UserController::class,'register']);
-Route::post('login',[UserController::class,'login']);
-Route::post('upload',[UserController::class,'addImage'])->middleware('auth:sanctum');
+Route::post('logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
+Route::post('upload', [UserController::class, 'addImage'])->middleware('auth:sanctum');
