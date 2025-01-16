8c16f58ecf78f78c214b7d230fed385a99ff775e
Mo-fawaz <mmmf.mmmf96@gmail.com>
2025-01-16 09:29:07 +0300

    comment the middleware

--- routes/api.php
+++ routes/api.php
@@ -50,7 +50,7 @@ Route::prefix('/auth')->group(function () {
 });
 Route::prefix('/user')->group(function () {
     route::middleware('auth:sanctum')->group(function () {
-        Route::middleware('verify')->group(function () {
+    //    Route::middleware('verify')->group(function () {
             Route::get('/all', [UserController::class, 'index']);
             Route::post('/me', [UserController::class, 'user_profile']);
             Route::get('/me', [UserController::class, 'user_profile']);
@@ -62,7 +62,7 @@ Route::prefix('/user')->group(function () {
             Route::get('show-deleted', [UserController::class, 'showDeleteUser']);
             Route::post('restore_user', [UserController::class, 'restoreUser']);
             Route::post('search', [UserController::class, 'searchUser']);
-        });
+   //     });
     });
 });
 Route::middleware('auth:sanctum')->group(function () {
