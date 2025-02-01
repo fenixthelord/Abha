93b36d06687c6e763daf5374b71125816132b68c
maher.salama <maher.salama@darrebni.net>
2025-02-01 17:10:24 +0300

    * update FormSubmissionVakue Model

--- app/Models/FormSubmissionValue.php
+++ app/Models/FormSubmissionValue.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FormSubmissionValue extends Model
 {
-    use HasFactory;
+    protected $fillable = ['form_submission_id', 'form_field_id', 'value'];
+
+    public function submission()
+    {
+        return $this->belongsTo(FormSubmission::class);
+    }
+
+    public function field()
+    {
+        return $this->belongsTo(FormField::class);
+    }
 }
