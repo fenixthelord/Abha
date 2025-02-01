91940af602c517e2d604c0dc17fbf84205cd07b4
maher.salama <maher.salama@darrebni.net>
2025-02-01 17:08:26 +0300

    * Update FormSubmission Model

--- app/Models/FormSubmission.php
+++ app/Models/FormSubmission.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FormSubmission extends Model
 {
-    use HasFactory;
+    protected $fillable = ['form_id'];
+
+    public function form()
+    {
+        return $this->belongsTo(Form::class);
+    }
+
+    public function values()
+    {
+        return $this->hasMany(FormSubmissionValue::class);
+    }
 }
