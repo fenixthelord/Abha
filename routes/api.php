1bce1be0b3d45e79c236ebe291a88587092f26ba
Ammar <ammarasafi041@gmail.com>
2025-02-01 11:22:40 +0300

    delete groups

--- routes/api.php
+++ routes/api.php
@@ -55,33 +55,33 @@ Route::prefix('/auth')->group(function () {
 
 Route::prefix('/user')->group(function () {
     route::middleware('auth:sanctum')->group(function () {
-     //   Route::middleware('activeVerify')->group(function () {
-            Route::get('/all', [UserController::class, 'index']);
-            Route::post('/me', [UserController::class, 'user_profile']);
-            Route::get('/me', [UserController::class, 'user_profile']);
-            Route::post('send', [UserController::class, 'sendOTP']);
-            Route::post('update-profile', [UserController::class, 'update']);
-            Route::post('update', [UserController::class, 'updateAdmin']);
-            Route::post('upload', [UserController::class, 'addImage']);
-            Route::post('delete-user', [UserController::class, 'deleteUser']);
-            Route::get('show-deleted', [UserController::class, 'showDeleteUser']);
-            Route::post('restore_user', [UserController::class, 'restoreUser']);
-            Route::post('search', [UserController::class, 'searchUser']);
-        });
-     //   });
-       // Route::middleware('activeVerify')->group(function () {
-            Route::get('/all', [UserController::class, 'index']);
-            Route::post('/me', [UserController::class, 'user_profile']);
-            Route::get('/me', [UserController::class, 'user_profile']);
-            Route::post('send', [UserController::class, 'sendOTP']);
-            Route::post('update-profile', [UserController::class, 'update']);
-            Route::post('update', [UserController::class, 'updateAdmin']);
-            Route::post('upload', [UserController::class, 'addImage']);
-            Route::post('delete-user', [UserController::class, 'deleteUser']);
-            Route::get('show-deleted', [UserController::class, 'showDeleteUser']);
-            Route::post('restore_user', [UserController::class, 'restoreUser']);
-            Route::post('search', [UserController::class, 'searchUser']);
-  //  });
+        //   Route::middleware('activeVerify')->group(function () {
+        Route::get('/all', [UserController::class, 'index']);
+        Route::post('/me', [UserController::class, 'user_profile']);
+        Route::get('/me', [UserController::class, 'user_profile']);
+        Route::post('send', [UserController::class, 'sendOTP']);
+        Route::post('update-profile', [UserController::class, 'update']);
+        Route::post('update', [UserController::class, 'updateAdmin']);
+        Route::post('upload', [UserController::class, 'addImage']);
+        Route::post('delete-user', [UserController::class, 'deleteUser']);
+        Route::get('show-deleted', [UserController::class, 'showDeleteUser']);
+        Route::post('restore_user', [UserController::class, 'restoreUser']);
+        Route::post('search', [UserController::class, 'searchUser']);
+    });
+    //   });
+    // Route::middleware('activeVerify')->group(function () {
+    Route::get('/all', [UserController::class, 'index']);
+    Route::post('/me', [UserController::class, 'user_profile']);
+    Route::get('/me', [UserController::class, 'user_profile']);
+    Route::post('send', [UserController::class, 'sendOTP']);
+    Route::post('update-profile', [UserController::class, 'update']);
+    Route::post('update', [UserController::class, 'updateAdmin']);
+    Route::post('upload', [UserController::class, 'addImage']);
+    Route::post('delete-user', [UserController::class, 'deleteUser']);
+    Route::get('show-deleted', [UserController::class, 'showDeleteUser']);
+    Route::post('restore_user', [UserController::class, 'restoreUser']);
+    Route::post('search', [UserController::class, 'searchUser']);
+    //  });
 });
 Route::middleware('auth:sanctum')->group(function () {
     Route::middleware('activeVerify')->group(function () {
@@ -95,25 +95,25 @@ Route::middleware('auth:sanctum')->group(function () {
 
 
 Route::prefix('roles-and-permissions')->middleware('auth:sanctum')->group(function () {
- //   Route::middleware('activeVerify')->group(function () {
-        Route::get('/', [RoleAndPermissionController::class, 'index']);
-        Route::post('/create', [RoleAndPermissionController::class, 'store']);
-        Route::post('/permission/create', [RoleAndPermissionController::class, 'CreatePermission']);
-        Route::put('/{id}', [RoleAndPermissionController::class, 'update']);
-        Route::delete('/{id}', [RoleAndPermissionController::class, 'destroy']);
-        Route::post('/roles/remove', [RoleAndPermissionController::class, 'RemovePermissionsFromRole']);
-        Route::post('/roles/assign', [RoleAndPermissionController::class, 'AssignPermissionsToRole']);
-        Route::post('/role/sync', [RoleAndPermissionController::class, 'SyncPermission']);
-        Route::post('roles/delete', [RoleAndPermissionController::class, 'DeleteRole']);
-        Route::get('permissions/get', [RoleAndPermissionController::class, 'GetAllPermissions']);
-        Route::prefix('users')->group(function () {
-            Route::post('/permissions', [RoleAndPermissionController::class, 'assignPermission']);
-            Route::post('/roles', [RoleAndPermissionController::class, 'assignRole']);
-            Route::post('/remove', [RoleAndPermissionController::class, 'removeRoleFromUser']);
-            Route::post('/direct/remove', [RoleAndPermissionController::class, 'RemoveDirectPermission']);
-            Route::post('/get', [RoleAndPermissionController::class, 'GetUserPermissions']);
-        });
- //   });
+    //   Route::middleware('activeVerify')->group(function () {
+    Route::get('/', [RoleAndPermissionController::class, 'index']);
+    Route::post('/create', [RoleAndPermissionController::class, 'store']);
+    Route::post('/permission/create', [RoleAndPermissionController::class, 'CreatePermission']);
+    Route::put('/{id}', [RoleAndPermissionController::class, 'update']);
+    Route::delete('/{id}', [RoleAndPermissionController::class, 'destroy']);
+    Route::post('/roles/remove', [RoleAndPermissionController::class, 'RemovePermissionsFromRole']);
+    Route::post('/roles/assign', [RoleAndPermissionController::class, 'AssignPermissionsToRole']);
+    Route::post('/role/sync', [RoleAndPermissionController::class, 'SyncPermission']);
+    Route::post('roles/delete', [RoleAndPermissionController::class, 'DeleteRole']);
+    Route::get('permissions/get', [RoleAndPermissionController::class, 'GetAllPermissions']);
+    Route::prefix('users')->group(function () {
+        Route::post('/permissions', [RoleAndPermissionController::class, 'assignPermission']);
+        Route::post('/roles', [RoleAndPermissionController::class, 'assignRole']);
+        Route::post('/remove', [RoleAndPermissionController::class, 'removeRoleFromUser']);
+        Route::post('/direct/remove', [RoleAndPermissionController::class, 'RemoveDirectPermission']);
+        Route::post('/get', [RoleAndPermissionController::class, 'GetUserPermissions']);
+    });
+    //   });
 });
 
 Route::get('/audit-logs', [AuditLogController::class, 'index']);
@@ -145,6 +145,7 @@ Route::prefix('notify-groups')->group(function () {
     Route::delete('/{notifyGroupId}/users', [NotifyGroupController::class, 'removeUsersFromNotifyGroup']);
 
     Route::post('/{notifyGroupId}/send-notification', [NotifyGroupController::class, 'sendNotificationToNotifyGroup']);
+    Route::delete('/{notifyGroupId}/delete', [NotifyGroupController::class, 'deleteNotifyGroup']);
 });
 
 Route::post('/notifications', [NotificationController::class, 'store'])
@@ -160,7 +161,7 @@ Route::get('/user/notifications', [NotificationController::class, 'getUserNotifi
  */
 
 Route::group(["prefix" => "/categories"], function () {
-    
+
     Route::get("/show", [CategoryController::class, "list"]);
     Route::get("/filter", [CategoryController::class, "filter"]);
     Route::post("/department/create", [CategoryController::class, "create"]);
