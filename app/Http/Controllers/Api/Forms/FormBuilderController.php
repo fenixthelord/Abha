8e0d031c3951642925e44f3b84e7a4b4264b8262
maher.salama <maher.salama@darrebni.net>
2025-02-02 21:19:10 +0300

    * CRUD in FormBuilderController

--- app/Http/Controllers/Api/Forms/FormBuilderController.php
+++ app/Http/Controllers/Api/Forms/FormBuilderController.php
@@ -11,6 +11,8 @@ use App\Models\Form;
 use App\Models\FormField;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules\Enum;
 
 class FormBuilderController extends Controller
@@ -34,16 +36,63 @@ class FormBuilderController extends Controller
         }
     }
 
-    public function show() {}
+    public function show($id)
+    {
+        try {
+            $form = Form::with('fields')->findOrFail($id);
+            $data['form'] =  FormResource::make($form);
+            return $this->returnData($data);
+        } catch (\Exception $e) {
+            return $this->handleException($e);
+        }
+    }
 
-    public function update() {}
+    public function update(Request $request, $id)
+    {
+        DB::beginTransaction();
+        try {
+            $request->validate([
+                'category_id' => 'required|numeric',
+                'name' => 'required|array',
+                'name.en' => 'required|string|min:2|max:255',
+                'name.ar' => 'required|string|min:2|max:255',
+                'fields' => 'required|array',
+                'fields.*.id' => 'required|string',
+                'fields.*.label.en' => 'required|string',
+                'fields.*.label.ar' => 'required|string',
+                'fields.*.placeholder.en' => 'required|string',
+                'fields.*.placeholder.ar' => 'required|string',
+                'fields.*.type' => ['required', new Enum(FormFiledType::class)],
+                'fields.*.options' => 'nullable|array',
+                'fields.*.required' => 'nullable|boolean',
+                'fields.*.order' => 'nullable|numeric',
+            ]);
 
-    public function destroy() {}
+            $form = Form::with('fields')->findOrFail($id);
+
+            $form->name = $request->name ?? $form->name;
+            $form->category_id = $request->category_id ?? $form->category_id;
+            $form->save();
+            foreach ($request->fields as $fieldData) {
+                $child = $form->fields()->find($fieldData['id']);
+                if ($child)
+                    $child->update($fieldData);
+            }
+            $data['form'] =  FormResource::make($form);
+            DB::commit();
+            $form->refresh();
+            return $this->returnData($data);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return $this->handleException($e);
+        }
+    }
 
     public function store(Request $request)
     {
         try {
             $request->validate([
+                'category_id' => 'required|numeric',
                 'name' => 'required|array',
                 'name.en' => 'required|string|min:2|max:255',
                 'name.ar' => 'required|string|min:2|max:255',
@@ -53,6 +102,9 @@ class FormBuilderController extends Controller
                 'fields.*.placeholder.en' => 'required|string',
                 'fields.*.placeholder.ar' => 'required|string',
                 'fields.*.type' => ['required', new Enum(FormFiledType::class)],
+                'fields.*.options' => 'nullable|array',
+                'fields.*.required' => 'nullable|boolean',
+                'fields.*.order' => 'nullable|numeric',
             ]);
 
             DB::beginTransaction();
@@ -76,6 +128,27 @@ class FormBuilderController extends Controller
 
             DB::commit();
             return $this->returnSuccessMessage("Form created successfully");
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return $this->handleException($e);
+        }
+    }
+
+    public function destroy($id)
+    {
+        DB::beginTransaction();
+        try {
+            $form = Form::onlyTrashed()->find($id);
+            if (!$form) {
+                return $this->badRequest('Form already deleted.');
+            } else {
+                $form = Form::findOrFail($id);
+                $form->name = $form->name . '-' . $form->id . '-deleted';
+                $form->save();
+                $form->delete();
+                DB::commit();
+                return $this->returnSuccessMessage('Form deleted successfully');
+            }
         } catch (\Exception $e) {
             DB::rollBack();
             return $this->handleException($e);
