9f922571cd9e78c30628a4e8ca46c257df104e74
Mo-fawaz <mmmf.mmmf96@gmail.com>
2025-02-05 09:35:02 +0300

    restore route

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -10,17 +10,18 @@ use App\Http\Controllers\Api\Auth\SocialLoginController;
 Route::prefix('auth')->group(function () {
     Route::middleware('activeVerify')->group(function () {
         Route::post('register', [UserAuthController::class, 'register'])->middleware('auth:sanctum');
-        Route::post('login', [UserAuthController::class, 'login']);
-        Route::post('/forgot-password', [ChangePasswordController::class, 'forgotPassword']);
-        Route::post('/reset-password', [ChangePasswordController::class, 'resetPassword']);
         Route::post('active', [UserController::class, 'active']);
-        Route::post('logout', [UserAuthController::class, 'logout']);
-        Route::post('refresh-token', [UserAuthController::class, 'refreshToken']);
-        Route::post('/link-social', [SocialLoginController::class, 'linkSocialAccount'])
-            ->name('auth.link-social');
         Route::middleware(['auth:sanctum'])->group(function () {
             Route::get('get-verify', [UserController::class, 'sendOtp']);
             Route::post('cheek-verify', [UserController::class, 'verifyOtp']);
         });
     });
+
+        Route::post('login', [UserAuthController::class, 'login']);
+        Route::post('/forgot-password', [ChangePasswordController::class, 'forgotPassword']);
+        Route::post('/reset-password', [ChangePasswordController::class, 'resetPassword']);
+        Route::post('logout', [UserAuthController::class, 'logout']);
+        Route::post('refresh-token', [UserAuthController::class, 'refreshToken']);
+        Route::post('/link-social', [SocialLoginController::class, 'linkSocialAccount'])
+            ->name('auth.link-social');
 });
