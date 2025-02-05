9bd16ae62cd513db4e58dd6abe44c970d89d5be9
Mo-fawaz <mmmf.mmmf96@gmail.com>
2025-02-05 09:30:11 +0300

    restore route

--- routes/api/roleAndPermissions.php
+++ routes/api/roleAndPermissions.php
@@ -10,21 +10,22 @@ Route::prefix('roles-and-permissions')->middleware('auth:sanctum')->group(functi
         Route::put('/{id}', [RoleAndPermissionController::class, 'update']);
         Route::delete('/{id}', [RoleAndPermissionController::class, 'destroy']);
         Route::prefix('roles')->group(function () {
-            Route::post('/', [RoleAndPermissionController::class, 'assignRole']);
             Route::post('/remove', [RoleAndPermissionController::class, 'RemovePermissionsFromRole']);
             Route::post('/assign', [RoleAndPermissionController::class, 'AssignPermissionsToRole']);
             Route::post('/delete', [RoleAndPermissionController::class, 'DeleteRole']);
         });
         Route::prefix('permissions')->group(function () {
-            Route::post('/', [RoleAndPermissionController::class, 'assignPermission']);
             Route::get('/get', [RoleAndPermissionController::class, 'GetAllPermissions']);
             Route::post('/create', [RoleAndPermissionController::class, 'CreatePermission']);
         });
         Route::post('role/sync', [RoleAndPermissionController::class, 'SyncPermission']);
         Route::prefix('users')->group(function () {
+            Route::post('/roles', [RoleAndPermissionController::class, 'assignRole']);
+            Route::post('/permissions', [RoleAndPermissionController::class, 'assignPermission']);
             Route::post('/remove', [RoleAndPermissionController::class, 'removeRoleFromUser']);
             Route::post('/direct/remove', [RoleAndPermissionController::class, 'RemoveDirectPermission']);
             Route::post('/get', [RoleAndPermissionController::class, 'GetUserPermissions']);
         });
     });
 });
+
