4c8e84f8e1097da760d120d90bc44675f010e07e
Mo-fawaz <mmmf.mmmf96@gmail.com>
2025-02-05 10:11:35 +0300

    restore route

--- routes/api/public.php
+++ routes/api/public.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use  App\Http\Controllers\Api\DatabaseController;
 use App\Http\Controllers\Api\ExcelController;
 use App\Http\Controllers\Api\LanguageController;
+use App\Http\Controllers\Api\AuditLogController;
 
 Route::group(["prefix" => "/db"], function () {
     Route::get('/tables', [DatabaseController::class, 'getTables']);
@@ -12,3 +13,4 @@ Route::group(["prefix" => "/db"], function () {
 Route::post('/extract-column', [ExcelController::class, 'extractColumn']);
 
 Route::get('lang/{locale}', [LanguageController::class, 'swap'])->middleware("changeLang");
+Route::get('/audit-logs', [AuditLogController::class, 'index'])->middleware('auth:sanctum');
