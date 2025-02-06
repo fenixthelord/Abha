a5fe070ecd0ce95070eb565ea56c9bdd7055f6de
maher.salama <maher.salama@darrebni.net>
2025-02-06 10:56:59 +0300

    * change resources to change the view for form traslations accrding to the current route

--- app/Http/Resources/Forms/FormFieldResource.php
+++ app/Http/Resources/Forms/FormFieldResource.php
@@ -9,10 +9,11 @@ class FormFieldResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $is_list = request()->route()->getName() === 'forms.list';
         return [
             'id' => $this->id,
             'order' => $this->order,
-            'label' => $this->label,
+            'label' => $is_list ? $this->label : $this->getTranslations("label"),
             'placeholder' => $this->placeholder,
             'type' => $this->type,
             'required' => $this->required,
