82f9a45a49133b001a7e7e6cd89e8803dda59241
Ammar <ammarasafi041@gmail.com>
2025-02-06 11:11:57 +0300

    event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+use Spatie\Translatable\HasTranslations;
 
-class Event extends Model
+class Event extends Model implements Auditable
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, \OwenIt\Auditing\Auditable, HasTranslations;
+
+    
 }
