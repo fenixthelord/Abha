6c9f63308ccf8be845a63816569561b2c3eaf4ff
Maria <maria.shahla@darrebni.net>
2025-02-09 15:04:36 +0300

    service notification

---
+++ app/Services/NotificationService.php
@@ -0,0 +1,31 @@
+<?php
+namespace app\services;
+use  Illuminate\Support\Facades\Http;
+class NotificationService
+{
+    protected $baseUrl;
+
+    public function __construct()
+    {
+        // Base URL for Smart ABHA Notification Service
+        $this->baseUrl = env('NOTIFY_SERV');
+
+    }
+
+    public function Postcall($method,$params)
+    {
+
+        $response = Http::WithHeaders([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ])->post("http://smartabha-notification.test/api".$method,$params);
+
+        return $response->json();
+    }
+    public function Getcall($method,$params)
+    {
+        $response = Http::get("{$this->baseUrl}/$method",$params);
+
+        return $response->json();
+    }
+}
