6167e007adc5c0d34bfd017e2c032ca2299ffd53
Ammar <ammarasafi041@gmail.com>
2025-02-09 11:27:34 +0300

    ..

--- database/migrations/2025_02_09_112518_make_file_nullable_in_events_table.php
+++ database/migrations/2025_02_09_112518_make_file_nullable_in_events_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->string("file")->nullable();
+            $table->string("file")->nullable()->change();
         });
     }
 
