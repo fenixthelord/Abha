7cb5fc5ed456ab09a11c5bb809cf262e009842af
Maria <maria.shahla@darrebni.net>
2025-02-10 14:51:15 +0300

    notification resource

---
+++ app/Http/Resources/Group/GroupResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\Group;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GroupResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+       return [
+           'id' => $this->resource['id'], // Change $this->id to $this->resource['id']
+           'name' => $this->resource['name'], // Adjust to $this->resource['name']
+           'description' => $this->resource['description'],
+           'group_model' => $this->resource['group_model'],
+           'owner_id' => $this->resource['owner_id'],
+           'department_id' => $this->resource['department_id'] ?? null,
+           'group_service' => $this->resource['group_service'],
+           'users' =>MemberResource::collection($this->resource['members']),
+
+
+       ];
+    }
+}
