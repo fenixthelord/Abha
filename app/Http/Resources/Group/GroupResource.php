968bd11fb4600fbe95daefdeedb9719dd80e829f
Maria <maria.shahla@darrebni.net>
2025-02-12 09:18:57 +0300

    group resource

--- app/Http/Resources/Group/GroupResource.php
+++ app/Http/Resources/Group/GroupResource.php
@@ -18,7 +18,7 @@ class GroupResource extends JsonResource
            'id' => $this->resource['id'], // Change $this->id to $this->resource['id']
            'name' => $this->resource['name'], // Adjust to $this->resource['name']
            'description' => $this->resource['description'],
-           'group_model' => $this->resource['group_model'],
+           'group_model' => $this->resource['group_type'],
            'owner_id' => $this->resource['owner_id'],
            'department_id' => $this->resource['department_id'] ?? null,
            'group_service' => $this->resource['group_service'],
