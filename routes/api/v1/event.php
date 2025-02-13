8f5cbc60abd24b24fb19b8a09c3a1421e56275ae
Mo-fawaz <mmmf.mmmf96@gmail.com>
2025-02-13 11:29:58 +0300

    add auth

--- routes/api/v1/event.php
+++ routes/api/v1/event.php
@@ -1,14 +1,18 @@
 <?php
-use App\Http\Controllers\Api\UploadFileController;
+
 use App\Http\Controllers\Api\Event\EventController;
+use App\Http\Controllers\Api\UploadFileController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post("upload/file" , [UploadFileController::class , "upload"]);
-
+Route::post("upload/file", [UploadFileController::class, "upload"]);
 Route::group(["prefix" => "/event"], function () {
     Route::get('/', [EventController::class, 'list']);
-    Route::post('/create', [EventController::class, 'createEvent']);
-    Route::put('/update', [EventController::class, 'updateEvent']);
-    Route::delete('/delete', [EventController::class, 'deleteEvent']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::middleware('activeVerify')->group(function () {
+            Route::post('/create', [EventController::class, 'createEvent']);
+            Route::put('/update', [EventController::class, 'updateEvent']);
+            Route::delete('/delete', [EventController::class, 'deleteEvent']);
+        });
+    });
 });
