6fc6d14ca699a2d4fc3688d1b63fd86b10c7acda
Qousai <qousai.alkourdi@darrebni.net>
2025-02-13 09:31:20 +0300

    ok

--- app/Http/Controllers/Api/Auth/UserAuthController.php
+++ app/Http/Controllers/Api/Auth/UserAuthController.php
@@ -47,12 +47,12 @@ class UserAuthController extends Controller
                 'image' => 'nullable|string',
                 'role' => 'nullable|array',
                 'role.*' => 'string|exists:roles,name',
-                'department_id' => ["nullable", "string", Rule::exists('departments', 'id')->where("deleted_at", null)],
+                'department_id' => ["required", "string", Rule::exists('departments', 'id')->where("deleted_at", null)],
             ], messageValidation());
             if ($validator->fails()) {
                 return $this->returnValidationError($validator);
             }
-      //      $department = Department::where("id", $request->department_id)->firstorFail();
+            $department = Department::where("id", $request->department_id)->firstorFail();
 
             $user = User::create([
                 'first_name' => $request->first_name,
