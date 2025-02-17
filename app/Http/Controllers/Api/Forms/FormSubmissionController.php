0d8df51fb27ad1101de6c9be5ade2f126d6a4ec0
Abdul-Rahman Al-Jabban <a.jabban2000@gmail.com>
2025-02-17 12:15:45 +0300

    modified FormSubmission::store()

--- app/Http/Controllers/Api/Forms/FormSubmissionController.php
+++ app/Http/Controllers/Api/Forms/FormSubmissionController.php
@@ -21,6 +21,7 @@ class FormSubmissionController extends Controller
             $form = Form::findOrFail($id);
             $rules = [];
 
+            // Create validation rules dynamically
             foreach ($form->fields as $field) {
                 if ($field->required) {
                     $rules["fields.{$field->label}"] = ['required'];
@@ -35,25 +36,26 @@ class FormSubmissionController extends Controller
                     $rules["fields.{$field->label}"][] = 'date';
                 }
             }
-
+            //Validate $request based on the created rules
             $validatedData = $request->validate($rules);
 
             // Save Submission
             $submission = FormSubmission::create(['form_id' => $form->id]);
 
             // Save Field Values
-            foreach ($validatedData['fields'] as $field_id => $value) {
-                $field = FormField::find($field_id);
+            foreach ($form->fields as $field) {
+                $value = $validatedData['fields'][$field->label];
+                $field = FormField::find($field->id);
 
                 if ($field) {
                     FormSubmissionValue::create([
                         'form_submission_id' => $submission->id,
-                        'form_field_id' => $field_id,
+                        'form_field_id' => $field->id,
                         'value' => is_array($value) ? json_encode($value) : $value,
                     ]);
                 } else {
                     // Handle non-existent form field
-                    return response()->json(['error' => "Form field with ID $field_id does not exist."], 400);
+                    return response()->json(['error' => "Form field with the ID you provided does not exist."], 400);
                 }
             }
 
