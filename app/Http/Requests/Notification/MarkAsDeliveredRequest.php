f3c7c002f63d300a423acab67cfc19394aaaefa8
reemgeorges <reem.georges@themetagate.me>
2025-02-18 08:53:05 +0300

    reader

--- app/Http/Requests/Notification/MarkAsDeliveredRequest.php
+++ app/Http/Requests/Notification/MarkAsDeliveredRequest.php
@@ -11,18 +11,44 @@ class MarkAsDeliveredRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true; // You can modify this if you need additional authorization checks.
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array
      */
     public function rules(): array
     {
         return [
-            //
+            'notification_id' => 'required', // Check if notification_id exists in the notifications table
+
         ];
+    }
+
+    /**
+     * Customize the error messages for failed validation.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'notification_id.required' => 'The notification ID is required.',
+
+
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param \Illuminate\Contracts\Validation\Validator $validator
+     * @return void
+     */
+    protected function failedValidation($validator)
+    {
+        throw new \HttpResponseException($this->returnValidationError($validator));
     }
 }
