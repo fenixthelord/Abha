a357d71c799d56afeca6d531a4a57891f5ed98f4
Mo-fawaz <mmmf.mmmf96@gmail.com>
2025-02-20 15:34:57 +0300

    remove auth

--- routes/api/v1/user.php
+++ routes/api/v1/user.php
@@ -15,7 +15,6 @@ Route::prefix('user')->group(function () {
             Route::get('show-deleted', [UserController::class, 'showDeleteUser']);
             Route::post('restore_user', [UserController::class, 'restoreUser']);
             Route::post('search', [UserController::class, 'searchUser']);
-            route::post('show', [UserController::class, 'show']);
         });
     });
 });
