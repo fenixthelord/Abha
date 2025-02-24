c6db82414b2753852c42ed92f0a5d4f915dfcac2
Abdul-Rahman Al-Jabban <a.jabban2000@gmail.com>
2025-02-24 10:11:38 +0300

    cleared cache after assigning permission to role

--- app/Http/Controllers/Api/RoleAndPermissionController.php
+++ app/Http/Controllers/Api/RoleAndPermissionController.php
@@ -163,6 +163,9 @@ class RoleAndPermissionController extends Controller
 
                 $role->givePermissionTo($request->permission);
 
+                // Clear the permissions cache after modifications
+                app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
                 $data['role'] = RolesResource::make($role);
                 return $this->returnData($data);
             }
