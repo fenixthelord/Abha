eadf50e9a8a5c7b9425c26eb96dbdcce58b92edd
Maria <maria.shahla@darrebni.net>
2025-03-01 12:21:54 +0300

    member resource

--- app/Http/Resources/Group/MemberResource.php
+++ app/Http/Resources/Group/MemberResource.php
@@ -15,31 +15,21 @@ class MemberResource extends JsonResource
 
     public function toArray(Request $request): array
     {
-        // Use either 'user' or 'customer' key from the resource
-        $data = [];
-        if (isset($this->resource['user'])) {
-            $data = $this->resource['user']['data'] ?? [];
-        } elseif (isset($this->resource['customer'])) {
-            $data = $this->resource['customer']['data'] ?? [];
-        }
-
         $firstName = '';
         $lastName  = '';
 
-        // Check member type if available
-        if (($this->resource['member_type'] ?? '') === 'customers') {
-            // For customer, the data should be under the "customer" key.
-            // Sometimes it might be an indexed array, sometimes an associative array.
-            if (isset($data['customer'])) {
-                // If it's indexed (i.e. multiple customers), take the first element.
-                $customerData = isset($data['customer'][0]) ? $data['customer'][0] : $data['customer'];
+        // Determine if the member is a customer or a user
+        if (($this->resource['member_type'] ?? '') === 'customers') { // Corrected from 'customers' to 'customer'
+            // Handle customer case
+            $customerData = $this->resource['user']['data']['customer'] ?? []; // Fix: It was under 'user'
+            if (!empty($customerData)) {
                 $firstName = $customerData['first_name'] ?? '';
                 $lastName  = $customerData['last_name'] ?? '';
             }
-        } else {
-            // For a user, assume the fields are in the "user" key.
-            if (isset($data['user'])) {
-                $userData = isset($data['user'][0]) ? $data['user'][0] : $data['user'];
+        } elseif (($this->resource['member_type'] ?? '') === 'users') { // Corrected from 'users' to 'user'
+            // Handle user case
+            $userData = $this->resource['user']['data']['user'] ?? $this->resource['user']['data'] ?? []; // Fix: Some responses may not have 'user' inside 'data'
+            if (!empty($userData)) {
                 $firstName = $userData['first_name'] ?? '';
                 $lastName  = $userData['last_name'] ?? '';
             }
